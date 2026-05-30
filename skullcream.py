5238c22f0f07bc6dcb471c76cdeb46f7ee3f482f
n4ss4u <nassaugato@gmail.com>
2026-05-30 00:06:45 -0400

    Add main modules with show banner function

---
+++ skullcream.py
@@ -0,0 +1,30 @@
+import colorama
+
+def static_banner():
+    print(f"{colorama.Fore.GREEN}             .--.           .---.        .-.{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}         .---|--|   .-.     |   |  .---. |~|    .--.{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      .--|{colorama.Fore.WHITE}==={colorama.Fore.GREEN}|  |---|_|--.__|   |--|:::| |~|-==-|{colorama.Fore.WHITE}=={colorama.Fore.GREEN}|---.{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      |{colorama.Fore.WHITE}%%{colorama.Fore.GREEN}|   |  |===| |~~|{colorama.Fore.WHITE}%%{colorama.Fore.GREEN}|   |--|   |_|~|    |  |{colorama.Fore.WHITE}___{colorama.Fore.GREEN}|-.{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      |  |   |  |{colorama.Fore.WHITE}==={colorama.Fore.GREEN}| |==|  |   |  |{colorama.Fore.WHITE}:::{colorama.Fore.GREEN}|=| |    |  |---|=|{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      |  |   |  |   |_|__|  |   |__|   | | |    |  |___| |{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      |{colorama.Fore.WHITE}~~{colorama.Fore.GREEN}|{colorama.Fore.WHITE}==={colorama.Fore.GREEN}|--|{colorama.Fore.WHITE}==={colorama.Fore.GREEN}|~|~~|%%|~~~|--|:::|{colorama.Fore.WHITE}={colorama.Fore.GREEN}|{colorama.Fore.WHITE}~{colorama.Fore.GREEN}|{colorama.Fore.WHITE}----{colorama.Fore.GREEN}|{colorama.Fore.WHITE}=={colorama.Fore.GREEN}|{colorama.Fore.WHITE}---{colorama.Fore.GREEN}|{colorama.Fore.WHITE}={colorama.Fore.GREEN}|{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}      ^--^---'--^---^-^--^--^---'--^---^-^-^-==-^--^---^-'{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.WHITE}       coded by n4ss4u from AGORA © 2026 ~ SkullCream v0.1{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}                  ╔═╗┬┌─┬ ┬┬  ┬  ╔═╗┬─┐┌─┐┌─┐┌┬┐{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}                  ╚═╗├┴┐│ ││  │  ║  ├┬┘├┤ ├─┤│││{colorama.Fore.RESET}")
+    print(f"{colorama.Fore.GREEN}                  ╚═╝┴ ┴└─┘┴─┘┴─┘╚═╝┴└─└─┘┴ ┴┴ ┴{colorama.Fore.RESET}")
+    print(f" ┌─────────────────────────────────────────────────────────────")
+    print(f" │                           ")
+    print(f" ├[01]- Email pattern{colorama.Fore.RESET}           ")
+    print(f" ├[02]- First name{colorama.Fore.RESET}              ")
+    print(f" ├[03]- Middle name{colorama.Fore.RESET}             ")
+    print(f" ├[04]- Last name 1{colorama.Fore.RESET}             ")
+    print(f" ├[05]- Last name 2{colorama.Fore.RESET}             ")
+    print(f" ├[06]- Birthday day{colorama.Fore.RESET}            ")
+    print(f" ├[07]- Birthday month{colorama.Fore.RESET}          ")
+    print(f" └[08]- Birthday year{colorama.Fore.RESET}           ")
+
+
+
+
+static_banner()
