fd2c4c9a4c5f464b9289defe67beec741503b331
n4ss4u <nassaugato@gmail.com>
2026-05-30 00:29:48 -0400

    Add logic to improve banner option selection

--- skullcream.py
+++ skullcream.py
@@ -1,6 +1,11 @@
 import colorama
+import os
 
-def static_banner():
+data = {"email_pattern": "", "first_name": "", "middle_name": "", "last_name_1": "", "last_name_2": "", "birthday_day": "", "birthday_month": "", "birthday_year": ""}
+check_exist_data = ""
+
+def main_banner():
+    os.system("clear")
     print(f"{colorama.Fore.GREEN}             .--.           .---.        .-.{colorama.Fore.RESET}")
     print(f"{colorama.Fore.GREEN}         .---|--|   .-.     |   |  .---. |~|    .--.{colorama.Fore.RESET}")
     print(f"{colorama.Fore.GREEN}      .--|{colorama.Fore.WHITE}==={colorama.Fore.GREEN}|  |---|_|--.__|   |--|:::| |~|-==-|{colorama.Fore.WHITE}=={colorama.Fore.GREEN}|---.{colorama.Fore.RESET}")
@@ -15,16 +20,38 @@ def static_banner():
     print(f"{colorama.Fore.GREEN}                  ╚═╝┴ ┴└─┘┴─┘┴─┘╚═╝┴└─└─┘┴ ┴┴ ┴{colorama.Fore.RESET}")
     print(f" ┌─────────────────────────────────────────────────────────────")
     print(f" │                           ")
-    print(f" ├[01]- Email pattern{colorama.Fore.RESET}           ")
-    print(f" ├[02]- First name{colorama.Fore.RESET}              ")
-    print(f" ├[03]- Middle name{colorama.Fore.RESET}             ")
-    print(f" ├[04]- Last name 1{colorama.Fore.RESET}             ")
-    print(f" ├[05]- Last name 2{colorama.Fore.RESET}             ")
-    print(f" ├[06]- Birthday day{colorama.Fore.RESET}            ")
-    print(f" ├[07]- Birthday month{colorama.Fore.RESET}          ")
-    print(f" └[08]- Birthday year{colorama.Fore.RESET}           ")
 
+    for i, (key, value) in enumerate(data.items(), start=1):
+        if value == "":
+            check_exist_data = f"{colorama.Fore.RED}x{colorama.Fore.RESET}"
+        else:
+            check_exist_data = f"{colorama.Fore.GREEN}✓{colorama.Fore.RESET}"
+            
+        print(f" ├[0{i}]- {key.capitalize().replace("_", " ")} {check_exist_data}")
 
+    option = input(f" {colorama.Fore.WHITE}└──> {colorama.Fore.RESET}")
+    return option 
 
+if __name__ == "__main__":
+    while True:
+        option = main_banner()
 
-static_banner()
+        if option == "01" or option == "1":
+            data["email_pattern"] = input(f" {colorama.Fore.WHITE}[Enter email pattern]> {colorama.Fore.RESET}")
+        elif option == "02" or option == "2":
+            data["first_name"] = input(f" {colorama.Fore.WHITE}[Enter first name]> {colorama.Fore.RESET}")
+        elif option == "03" or option == "3":
+            data["middle_name"] = input(f" {colorama.Fore.WHITE}[Enter middle name]> {colorama.Fore.RESET}")
+        elif option == "04" or option == "4":
+            data["last_name_1"] = input(f" {colorama.Fore.WHITE}[Enter first last name]> {colorama.Fore.RESET}")
+        elif option == "05" or option == "5":
+            data["last_name_2"] = input(f" {colorama.Fore.WHITE}[Enter second last name]> {colorama.Fore.RESET}")
+        elif option == "06" or option == "6":
+            data["birthday_day"] = input(f" {colorama.Fore.WHITE}[Enter birthday day]> {colorama.Fore.RESET}")
+        elif option == "07" or option == "7":
+            data["birthday_month"] = input(f" {colorama.Fore.WHITE}[Enter birthday month]> {colorama.Fore.RESET}")
+        elif option == "08" or option == "8":
+            data["birthday_year"] = input(f" {colorama.Fore.WHITE}[Enter birthday year]> {colorama.Fore.RESET}")
+        elif option.lower() == "exit":
+            print(f"{colorama.Fore.GREEN}Exiting...{colorama.Fore.RESET}")
+            break
